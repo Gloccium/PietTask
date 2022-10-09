697f528d3a3e6b961b9e9739b40acbe416e56ea1
Gloccium <kurururunku@gmail.com>
2022-10-10 00:23:12 +0500

    stack finished

--- interpreter_components/rotation.py
+++ interpreter_components/rotation.py
@@ -3,3 +3,6 @@ import enum
 
 class Rotation(enum.Enum):
     UP, RIGHT, DOWN, LEFT = range(4)
+
+    def __add__(self, other):
+        return self.value + other
--- interpreter_components/stack.py
+++ interpreter_components/stack.py
@@ -7,4 +7,117 @@ class Stack:
         self.direction_pointer = Rotation.RIGHT
         self.codel_chooser = Rotation.LEFT
 
-    
+    def push(self, value):
+        self._stack.append(value)
+
+    def pop(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        self._stack.pop()
+
+    def add(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        x = self._stack.pop()
+        y = self._stack.pop()
+        self._stack.append(x + y)
+
+    def subtract(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        x = self._stack.pop()
+        y = self._stack.pop()
+        self._stack.append(y - x)
+
+    def multiply(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        x = self._stack.pop()
+        y = self._stack.pop()
+        self._stack.append(x * y)
+
+    def divide(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        x = self._stack.pop()
+        y = self._stack.pop()
+        if y == 0:
+            raise ZeroDivisionError
+        self._stack.append(x // y)
+
+    def mod(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        x = self._stack.pop()
+        y = self._stack.pop()
+        if y == 0:
+            raise ZeroDivisionError
+        self._stack.append(x % y)
+
+    def not_operation(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        value = self._stack.pop()
+        if value == 0:
+            self._stack.append(1)
+        self._stack.append(0)
+
+    def greater(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        x = self._stack.pop()
+        y = self._stack.pop()
+        if y > x:
+            self._stack.append(1)
+        else:
+            self._stack.append(0)
+
+    def pointer(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        value = self._stack.pop()
+        self.direction_pointer = Rotation((self.direction_pointer.value
+                                           + value) % 4)
+
+    def switch(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        value = self._stack.pop()
+        self.codel_chooser = Rotation((self.codel_chooser.value
+                                       + 2 * value) % 4)
+
+    def duplicate(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        self._stack.append(self._stack[-1])
+
+    def roll(self):
+        if len(self._stack) < 2:
+            raise ValueError('Stack does not contain enough items')
+        number = self._stack.pop()
+        depth = self._stack.pop()
+        number %= depth
+        if depth <= 0 or number == 0:
+            return
+        x = -abs(number) + depth * (number < 0)
+        self._stack[-depth:] = self._stack[x:] + self._stack[-depth:x]
+
+    def number_in(self):
+        number = int(input("Type in an integer number: "))
+        self._stack.append(number)
+
+    def char_in(self):
+        char = ord(input("Type in a character: "))
+        self._stack.append(char)
+
+    def number_out(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        value = self._stack.pop()
+        print(str(value), end="")
+
+    def char_out(self):
+        if len(self._stack) < 1:
+            raise ValueError('Stack does not contain enough items')
+        value = self._stack.pop()
+        print(chr(value), end="")
