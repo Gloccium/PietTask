379e8bf00561d10837d7f2be66c328abe697fc26
Gloccium <kurururunku@gmail.com>
2022-10-10 23:54:23 +0500

    path updated

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import io
+from pathlib import Path
 from PIL import Image
 from interpreter_components.stack import Stack
 from interpreter_components.interpreter import Interpreter
@@ -10,8 +11,8 @@ class TestPietInterpreter(unittest.TestCase):
     def test_hello_world(self):
         sys.stdout = io.StringIO()
         stack = Stack()
-        with Image.open(f'../images_to_interpret/HelloWorld.png')\
-                .convert('RGB') as image:
+        path = Path(Path.cwd(), 'test_images', 'HelloWorld.png')
+        with Image.open(path).convert('RGB') as image:
             interpreter = Interpreter(stack, image)
             interpreter.start()
         self.assertEqual('Hello world!', sys.stdout.getvalue())
@@ -19,16 +20,16 @@ class TestPietInterpreter(unittest.TestCase):
     def test_error_color(self):
         stack = Stack()
         with self.assertRaises(ValueError):
-            with Image.open(f'../images_to_interpret/ColorError.png')\
-                    .convert('RGB') as image:
+            path = Path(Path.cwd(), 'test_images', 'ColorError.png')
+            with Image.open(path).convert('RGB') as image:
                 interpreter = Interpreter(stack, image)
                 interpreter.start()
 
     def test_white_color(self):
         sys.stdout = io.StringIO()
         stack = Stack()
-        with Image.open(f'Add.png')\
-                .convert('RGB') as image:
+        path = Path(Path.cwd(), 'test_images', 'Add.png')
+        with Image.open(path).convert('RGB') as image:
             interpreter = Interpreter(stack, image)
             interpreter.start()
         self.assertEqual('4', sys.stdout.getvalue())
