6cbcfb00b74d74b831e7a200ef679aee7ae0d034
Gloccium <kurururunku@gmail.com>
2022-10-10 21:49:28 +0500

    rollback

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,7 +1,6 @@
 import unittest
 import sys
 import io
-from os import path
 from PIL import Image
 from interpreter_components.stack import Stack
 from interpreter_components.interpreter import Interpreter
@@ -11,9 +10,8 @@ class TestPietInterpreter(unittest.TestCase):
     def test_hello_world(self):
         sys.stdout = io.StringIO()
         stack = Stack()
-        with Image.open(
-                path.join('test_images', 'HelloWorld.png')).convert('RGB')\
-                as image:
+        with Image.open(f'../images_to_interpret/HelloWorld.png')\
+                .convert('RGB') as image:
             interpreter = Interpreter(stack, image)
             interpreter.start()
         self.assertEqual('Hello world!', sys.stdout.getvalue())
@@ -21,16 +19,16 @@ class TestPietInterpreter(unittest.TestCase):
     def test_error_color(self):
         stack = Stack()
         with self.assertRaises(ValueError):
-            with Image.open(path.join('test_images', 'ColorError.png')).\
-                    convert('RGB') as image:
+            with Image.open(f'../images_to_interpret/ColorError.png')\
+                    .convert('RGB') as image:
                 interpreter = Interpreter(stack, image)
                 interpreter.start()
 
     def test_white_color(self):
         sys.stdout = io.StringIO()
         stack = Stack()
-        with Image.open(path.join('test_images', 'Add.png')).convert('RGB')\
-                as image:
+        with Image.open(f'../images_to_interpret/Add.png')\
+                .convert('RGB') as image:
             interpreter = Interpreter(stack, image)
             interpreter.start()
         self.assertEqual('4', sys.stdout.getvalue())
